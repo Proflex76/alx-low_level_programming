cc1817a323477d4a84ac2e80d3026bb67c39be57
Proflex76 <franktebogofmathebula@gmail.com>
2023-07-26 14:55:36 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcat - concentratestwo strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+
+	int k;
+	int n;
+
+	k = 0;
+	while (dest[k] != '\0')
+	{
+		k++;
+	}
+	n = 0;
+	while (src[n] != '\0')
+	{
+		dest[k] = src[n];
+		k++;
+		n++;
+	}
+
+	dest[k] = '\0';
+	return (dest);
+
+}
+
