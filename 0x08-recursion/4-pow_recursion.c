57e2aff1900f8628f14de157a66626c9096fb198
Proflex76 <franktebogofmathebula@gmail.com>
2023-08-02 08:41:45 +0000

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates value of x raised to the power of y
+ * @x: given number
+ * @y: exponent
+ * Return: value raised multiplied y times
+ */
+
+int _pow_recursion(int x, int y)
+
+{
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+
+}
