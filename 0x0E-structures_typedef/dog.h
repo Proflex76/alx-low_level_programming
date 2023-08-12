70dbcba1b93ac853a8f47ae31f039becda48c16c
Proflex76 <franktebogofmathebula@gmail.com>
2023-08-12 18:41:40 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,13 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Seconds member
- * @owner: Third member
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  *
- * Description: Longer description
+ * Description: The attributes of the dog.
  */
 
 struct dog
@@ -16,19 +16,17 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 /**
- * @dog_t: typedef for struct dog
+ * dog_t - typedef for dog structure
  */
 
-dog_t; typedef struct dog_t
+typedef struct dog dog_t;
 
-int_dog(struct dog_t *d, char *name, float age, char *owner);
-void printf_dog(struct dog *d);
-struct dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(struct dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-#endif /* struct dog - a structure of dog */
+#endif
